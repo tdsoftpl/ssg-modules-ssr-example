84bb161e6802ff6e605139eda7922c45faf72715
Adrian Celczynski <adriancelczynski@icloud.com>
2022-02-08 15:18:47 +0100

    fix: nextjs eslint warnings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import "../styles/globals.css";
 import type {AppProps} from "next/app";
+import Head from "next/head";
 
 function MyApp({Component, pageProps}: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>SSG-Modules NextJS Example</title>
+                <meta
+                    name="description"
+                    content="An example NextJS app integrating the SSG-Modules packages using SSR."
+                />
+            </Head>
+
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,18 +11,11 @@ class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <meta charSet="UTF-8" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
                     <link rel="icon" href="spotlight-symbol.svg" />
                     <link
                         rel="stylesheet"
                         href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css"
-                    />
-
-                    <title>SSG-Modules NextJS Example</title>
-                    <meta
-                        name="description"
-                        content="An example NextJS app integrating the SSG-Modules packages using SSR."
                     />
                 </Head>
                 <body>
